a8ba5caad2222950560741ca8fd6baf628db1951
nnoldman <1527053436@qq.com>
2016-05-26 16:06:51 +0800

    11

---
+++ CmdHelper/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CmdHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cmdList = new System.Windows.Forms.TreeView();
+            this.console = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.cmdList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.console);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1204, 487);
+            this.splitContainer1.SplitterDistance = 203;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // cmdList
+            // 
+            this.cmdList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmdList.Location = new System.Drawing.Point(0, 0);
+            this.cmdList.Name = "cmdList";
+            this.cmdList.Size = new System.Drawing.Size(203, 487);
+            this.cmdList.TabIndex = 0;
+            this.cmdList.DoubleClick += new System.EventHandler(this.OnDoubleClick);
+            // 
+            // console
+            // 
+            this.console.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.console.Location = new System.Drawing.Point(0, 68);
+            this.console.Multiline = true;
+            this.console.Name = "console";
+            this.console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.console.Size = new System.Drawing.Size(997, 419);
+            this.console.TabIndex = 0;
+            this.console.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mouseup);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(991, 59);
+            this.panel1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(199, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "lock";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "清空";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 487);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnFormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView cmdList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox console;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ CmdHelper/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CmdHelper
+{
+    public partial class Form1 : Form
+    {
+        public class StartInfo
+        {
+            public string Name;
+            public string Args;
+        }
+
+        int mCurrentIndex = 0;
+        Process mCurrent;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitCmdList();
+
+            this.DoubleBuffered = true;
+            SetStyle(ControlStyles.ResizeRedraw |
+              ControlStyles.OptimizedDoubleBuffer |
+              ControlStyles.AllPaintingInWmPaint, true);
+            UpdateStyles();
+        }
+
+        public void InitCmdList()
+        {
+            StartInfo[] cmds = new StartInfo[]
+            {
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="devices",            },
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="tcpip 5555",            },
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="connect 192.168.1.102", },
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="start-server",      },
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="kill-server",       },
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="logcat",                },
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="logcat -c",             },
+             new StartInfo(){Name="D:/sdk/adt-bundle-windows-x86_64-20130917/adt-bundle-windows-x86_64-20130917/sdk/platform-tools/adb",Args="logcat -d > logcat.txt",},
+            };
+            foreach(var cmd in cmds)
+            {
+                var node = this.cmdList.Nodes.Add(cmd.Args);
+                node.Tag = cmd;
+            }
+        }
+        //process.WaitForExit();
+        void Clear()
+        {
+            if (mCurrent != null)
+            {
+                mCurrent.ErrorDataReceived -= OnDataReceived;
+                mCurrent.OutputDataReceived -= OnDataReceived;
+                mCurrent.EnableRaisingEvents = false;
+            }
+        }
+        private void OnDoubleClick(object sender, EventArgs e)
+        {
+            Clear();
+
+            TreeNode node = this.cmdList.SelectedNode;
+        
+            StartInfo info = (StartInfo)node.Tag;
+            mCurrent = new Process();
+            mCurrent.StartInfo = new ProcessStartInfo();
+            mCurrent.StartInfo.FileName = info.Name;
+            mCurrent.StartInfo.Arguments = info.Args;
+            mCurrent.StartInfo.CreateNoWindow = true;
+            mCurrent.StartInfo.RedirectStandardError = true;
+            mCurrent.StartInfo.RedirectStandardInput = true;
+            mCurrent.StartInfo.RedirectStandardOutput = true;
+            mCurrent.StartInfo.UseShellExecute = false;
+            mCurrent.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            mCurrent.EnableRaisingEvents = true;
+            mCurrent.ErrorDataReceived += OnDataReceived;
+            mCurrent.OutputDataReceived += OnDataReceived;
+            mCurrent.Start();
+            SetText(mCurrent.StartInfo.FileName + " " + mCurrent.StartInfo.Arguments);
+            mCurrent.BeginOutputReadLine();
+        }
+        
+        delegate void SetTextCallBack(string text);
+
+        public void OnDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                SetText(e.Data);
+            }
+        }
+        public void SetText(string text)
+        {
+            if (this.console.InvokeRequired)
+            {
+                SetTextCallBack stcb = new SetTextCallBack(SetText);
+                this.Invoke(stcb, new object[] { text });
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(text))
+                    this.console.AppendText(text + "\r\n");
+                
+                bool lockScrollBar = this.checkBox1.Checked;
+                
+                if (!lockScrollBar)
+                {
+                    console.SelectionStart = console.Text.Length;
+                    console.ScrollToCaret(); 
+                }
+                else
+                {
+                    console.SelectionStart = mCurrentIndex;
+                    console.ScrollToCaret();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.console.Clear();
+        }
+
+        private void mouseup(object sender, MouseEventArgs e)
+        {
+            int idx = this.console.GetCharIndexFromPosition(new Point(e.X, e.Y));
+            mCurrentIndex = idx;
+            console.SelectionStart = mCurrentIndex;
+            console.ScrollToCaret();
+        }
+
+        private void OnFormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Clear();
+        }
+    }
+}
